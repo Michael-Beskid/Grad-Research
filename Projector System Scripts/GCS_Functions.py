2ddf6bcd76688cbd6d9dde5db8610bc829b35863
Michael Beskid <73857255+Michael-Beskid@users.noreply.github.com>
2023-11-02 15:28:02 -0400

    Updated image generation

--- Projector System Scripts/GCS_Functions.py
+++ Projector System Scripts/GCS_Functions.py
@@ -2,6 +2,77 @@ import socket
 import base64
 import io
 from PIL import Image
+import colorsys
+import math
+import numpy as np
+
+
+class Point: 
+    def __init__(self, x, y): 
+        self.x = x
+        self.y = y
+
+
+def generate_image(height, width, mu_x, mu_y, sigma_x, sigma_y, saturation, value):
+
+    filename = "test_dist.png"
+
+    pixelArray = [[0 for i in range(width)] for j in range(height)]
+    for x in range(height):
+        for y in range(width):
+            pixelArray[x][y] = hue2rgb(xyFunction(x, y, mu_x, mu_y, sigma_x, sigma_y), saturation, value)
+
+    img = Image.fromarray(np.array(pixelArray), mode="RGB")
+    img.save(filename)
+
+
+def generate_image_with_rot(height, width, mu_x, mu_y, sigma_x, sigma_y, saturation, value, theta, scale_factor, invert):
+
+    filename = "test_dist.png"
+
+    pixelArray = [[0 for i in range(width)] for j in range(height)]
+    for x in range(height):
+        for y in range(width):
+            newPoint = calc_rot_point(x, y, mu_x, mu_y, theta)
+            pixelArray[x][y] = hue2rgb(xyFunction(newPoint.x, newPoint.y, mu_x, mu_y, sigma_x, sigma_y), saturation, value, scale_factor, invert)
+
+    img = Image.fromarray(np.array(pixelArray), mode="RGB")
+    img.save(filename)
+
+
+def calc_rot_point(x,y, mu_x, mu_y, theta):
+
+    # Translate mean to origin
+    newX = x - mu_x
+    newY = y - mu_y
+
+    # Perform rotation
+    newX = newX * math.cos(theta) - newY * math.sin(theta)
+    newY = newX * math.sin(theta) + newY * math.cos(theta)
+
+    # Translate mean back to original position
+    newX = newX + mu_x
+    newY = newY + mu_y
+
+    return Point(newX, newY)
+
+
+def xyFunction(x, y, mu_x, mu_y, sigma_x, sigma_y):
+    A = 2 * math.pi * (sigma_x ** 2) * (sigma_y ** 2)
+    B = (x - mu_x) ** 2
+    C = 2 * (sigma_x ** 2)
+    D = (y - mu_y) ** 2
+    F = 2 * (sigma_y ** 2)
+    return math.exp(-((B/C) + (D/F)))
+
+
+def hue2rgb(hue, saturation, value, scale_factor, invert):
+    if invert:
+        hue = scale_factor - hue*scale_factor
+    else:
+        hue = hue*scale_factor
+    return tuple(np.uint8(component * 255) for component in colorsys.hsv_to_rgb(hue, saturation, value))
+
 
 def split_image(image_path):
 
@@ -12,7 +83,7 @@ def split_image(image_path):
     width, height = original_image.size
 
     # Calculate the size of each cut image (1280x800)
-    cut_width, cut_height = 1280, 800
+    cut_width, cut_height = width/2, height/2
 
     #Define empty binary images
     binary_images = []
@@ -56,7 +127,7 @@ def Encode_Image(data):
 def UDP_SEND(Encoded_Image_1,Encoded_Image_2,Encoded_Image_3,Encoded_Image_4):
     while True:
         #Send to first PI
-        UDP_IP_1 = "192.168.1.80"
+        UDP_IP_1 = "192.168.1.11"
         UDP_PORT = 16690
         print(f"UDP target IP: {UDP_IP_1}"),
         print (f"UDP target port: {UDP_PORT}"),
@@ -66,7 +137,7 @@ def UDP_SEND(Encoded_Image_1,Encoded_Image_2,Encoded_Image_3,Encoded_Image_4):
         print("Message1 Sent")
         
         #Send to second PI
-        UDP_IP_2 = "192.168.1.80"
+        UDP_IP_2 = "192.168.1.12"
         print(f"UDP target IP: {UDP_IP_2}"),
         print (f"UDP target port: {UDP_PORT}"),
         
@@ -75,7 +146,7 @@ def UDP_SEND(Encoded_Image_1,Encoded_Image_2,Encoded_Image_3,Encoded_Image_4):
         print("Message2 Sent")
         
         #Send to third PI
-        UDP_IP_3 = "192.168.1.80"
+        UDP_IP_3 = "192.168.1.13"
         print(f"UDP target IP: {UDP_IP_3}"),
         print (f"UDP target port: {UDP_PORT}"),
         
@@ -84,7 +155,7 @@ def UDP_SEND(Encoded_Image_1,Encoded_Image_2,Encoded_Image_3,Encoded_Image_4):
         print("Message3 Sent")
         
         #Send to fourth PI
-        UDP_IP_4 = "192.168.1.80"
+        UDP_IP_4 = "192.168.1.14"
         print(f"UDP target IP: {UDP_IP_4}"),
         print (f"UDP target port: {UDP_PORT}"),
         
--- Projector System Scripts/UDP_GCS_script.py
+++ Projector System Scripts/UDP_GCS_script.py
@@ -1,4 +1,5 @@
 ## This script is meant to be run on the ground control station to publish the images to the headless PIs via UDP
+import math
 import GCS_Functions as GCS
 
 #Load Image From Image Generator
@@ -6,8 +7,29 @@ import GCS_Functions as GCS
 #    data = file.read()
 #print(len(data))
 
-# Call function to split projection image and return 4 PNGs
-Images = GCS.split_image('projection.png')
+# NOTE: Need to cut image size in half for this to work at the moment... increase buffer or use different method than UDP
+
+# THREAT FIELD PARAMETERS
+img_height = 800
+img_width = 1280
+mu_x = 250
+mu_y = 300
+sigma_x = 150
+sigma_y = 400
+saturation = 0.8
+value = 1.0
+theta = math.pi/12
+scale_factor = 0.8
+invert = False
+
+
+# Generate Gaussian threat field
+# GCS.generate_image(800, 1280, 250, 400, 200, 400, 0.6, 0.9)   # Good distribution
+# GCS.generate_image(800, 1280, 100, 100, 60, 180, 0.8, 1.0)      # Temp distribution for 1 projector
+GCS.generate_image_with_rot(img_height, img_width, mu_x, mu_y, sigma_x, sigma_y, saturation, value, theta, scale_factor, invert)      # Temp distribution for 1 projector
+
+# Split image and return 4 PNGs
+Images = GCS.split_image('test_dist.png')
 
 # Call encoding function to prepare image data for UDP transmission
 UDP_Message = GCS.Encode_Image(Images)
