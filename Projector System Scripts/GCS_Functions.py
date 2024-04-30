52bc6742c6c13c037b13096a2b53b2b3f9da64e3
Michael Beskid <73857255+Michael-Beskid@users.noreply.github.com>
2024-04-30 11:09:48 -0400

    Did some stuff I guess

--- Projector System Scripts/GCS_Functions.py
+++ Projector System Scripts/GCS_Functions.py
@@ -1,6 +1,3 @@
-import socket
-import base64
-import io
 from PIL import Image
 import colorsys
 import math
@@ -88,6 +85,8 @@ def split_image(image_path):
     #Define empty binary images
     binary_images = []
 
+    img_num = 0
+
     # Create four cut images
     for row in range(2):
         for col in range(2):
@@ -100,66 +99,12 @@ def split_image(image_path):
             # Crop the region and save as a new PNG
             split_image = original_image.crop((left, upper, right, lower))
 
-            # Create a BytesIO object to store the binary data
-            img_byte_array = io.BytesIO()
+            # Flip images 1 and 2 vertically due to projector setup
+            if row == 0:
+                split_image = split_image.transpose(Image.ROTATE_180)
 
-            # Save the cropped image to the BytesIO object
-            split_image.save(img_byte_array, format='PNG')
+            img_num += 1
+            filename = "/var/nfs/projector-pics/projector-" + str(img_num) + ".png"
 
-            # Append the binary data to the list
-            binary_images.append(img_byte_array.getvalue())
-
-    return binary_images
-
-
-
-# Function to encode in base64 format for UDP transmission
-def Encode_Image(data):
+            split_image.save(filename)
     
-    Encoded_Image_1 = base64.b64encode(data[0])
-    Encoded_Image_2 = base64.b64encode(data[1])
-    Encoded_Image_3 = base64.b64encode(data[2])
-    Encoded_Image_4 = base64.b64encode(data[3])
-    return(Encoded_Image_1,Encoded_Image_2,Encoded_Image_3,Encoded_Image_4)
-
-
-#Function to send image to selected UDP port
-def UDP_SEND(Encoded_Image_1,Encoded_Image_2,Encoded_Image_3,Encoded_Image_4):
-    while True:
-        #Send to first PI
-        UDP_IP_1 = "192.168.1.11"
-        UDP_PORT = 16690
-        print(f"UDP target IP: {UDP_IP_1}"),
-        print (f"UDP target port: {UDP_PORT}"),
-        
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-        sock.sendto(Encoded_Image_1, (UDP_IP_1, UDP_PORT))
-        print("Message1 Sent")
-        
-        #Send to second PI
-        UDP_IP_2 = "192.168.1.12"
-        print(f"UDP target IP: {UDP_IP_2}"),
-        print (f"UDP target port: {UDP_PORT}"),
-        
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-        sock.sendto(Encoded_Image_2, (UDP_IP_2, UDP_PORT))
-        print("Message2 Sent")
-        
-        #Send to third PI
-        UDP_IP_3 = "192.168.1.13"
-        print(f"UDP target IP: {UDP_IP_3}"),
-        print (f"UDP target port: {UDP_PORT}"),
-        
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-        sock.sendto(Encoded_Image_3, (UDP_IP_3, UDP_PORT))
-        print("Message3 Sent")
-        
-        #Send to fourth PI
-        UDP_IP_4 = "192.168.1.14"
-        print(f"UDP target IP: {UDP_IP_4}"),
-        print (f"UDP target port: {UDP_PORT}"),
-        
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-        sock.sendto(Encoded_Image_4, (UDP_IP_4, UDP_PORT))
-        print("Message4 Sent")
-        
